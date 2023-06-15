14e39eebcec1066a0e6887a9b989f94029fc9454
alexeirybak <128369349+alexeirybak@users.noreply.github.com>
2023-06-15 23:09:13 +0300

    Changes
    And now?

--- api.js
+++ api.js
@@ -1,10 +1,10 @@
 // Замени на свой, чтобы получить независимый от других набор данных.
 // "боевая" версия инстапро лежит в ключе prod
-const personalKey = "prod";
+const personalKey = "AZR";
 const baseHost = "https://webdev-hw-api.vercel.app";
 const postsHost = `${baseHost}/api/v1/${personalKey}/instapro`;
 
-export function getPosts({ token }) {
+export async function getPosts({ token }) {
   return fetch(postsHost, {
     method: "GET",
     headers: {
@@ -15,16 +15,28 @@ export function getPosts({ token }) {
       if (response.status === 401) {
         throw new Error("Нет авторизации");
       }
-
       return response.json();
     })
     .then((data) => {
-      return data.posts;
+// Получаем сразу все свойства поста
+      return data.posts.map((post) => {
+        return {
+          name: post.user?.name,
+          description: post.description,
+          time: post.createdAt,
+          postImg: post.imageUrl,
+          userImg: post.user?.imageUrl,
+          id: post.user.id,
+          idPost: post.id,
+          isLiked: post.isLiked,
+          likes: post.likes.length,
+          whoseLike: post.likes[0]?.name,
+        };
+      });
     });
 }
 
-// https://github.com/GlebkaF/webdev-hw-api/blob/main/pages/api/user/README.md#%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%D1%81%D1%8F
-export function registerUser({ login, password, name, imageUrl }) {
+export async function registerUser({ login, password, name, imageUrl }) {
   return fetch(baseHost + "/api/user", {
     method: "POST",
     body: JSON.stringify({
@@ -41,7 +53,7 @@ export function registerUser({ login, password, name, imageUrl }) {
   });
 }
 
-export function loginUser({ login, password }) {
+export async function loginUser({ login, password }) {
   return fetch(baseHost + "/api/user/login", {
     method: "POST",
     body: JSON.stringify({
@@ -57,7 +69,7 @@ export function loginUser({ login, password }) {
 }
 
 // Загружает картинку в облако, возвращает url загруженной картинки
-export function uploadImage({ file }) {
+export async function uploadImage({ file }) {
   const data = new FormData();
   data.append("file", file);
 
@@ -68,3 +80,82 @@ export function uploadImage({ file }) {
     return response.json();
   });
 }
+
+// Отправляем POST-запрос на сервер для добавления нового поста
+
+export async function addPost({ token, description, imageUrl }) {
+  return fetch(postsHost, {
+    method: "POST",
+    headers: {
+      Authorization: token,
+    },
+    body: JSON.stringify({
+      description,
+      imageUrl,
+    }),
+  }).then((response) => {
+    if(response.status === 400) {
+      throw new Error ('Картинку нужно описать и прикрепить')
+    }
+    return response.json();
+  })
+};
+
+// Получаем список постов пользователя с заданным идентификатором
+
+export async function getUserPosts(userId, token) {
+  return fetch(postsHost + "/user-posts/" + userId, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((response) => {
+      if (response.status === 401) {
+        throw new Error("Нет авторизации");
+      }
+
+      return response.json();
+    })
+    .then((data) => {
+      return data.posts.map((post) => {
+        return {
+          name: post?.user?.name,
+          description: post.description,
+          time: post.createdAt,
+          postImg: post.imageUrl,
+          userImg: post?.user?.imageUrl,
+          id: post.user?.id,
+          idPost: post.idPost,
+          isLiked: post.isLiked,
+          likes: post.likes.length,
+          whoseLike: post?.likes[0]?.name,
+        }
+      });
+    });
+  };
+
+  // Отправляем POST-запрос на сервер для поста, которому нужно добавить лайк
+  export function addLike({ token, idPost }) {
+    return fetch(postsHost + "/" + idPost + "/like", {
+      method: "POST",
+      headers: {
+        Authorization: token,
+      },
+      body: JSON.stringify({
+        idPost,
+      }),
+    })
+  };
+// Отправляем POST-запрос на сервер для поста, которому нужно добавить дизлайк
+  export function addDislike({ token, idPost }) {
+    return fetch(postsHost + "/" + idPost + "/dislike", {
+      method: "POST",
+      headers: {
+        Authorization: token,
+      },
+      body: JSON.stringify({
+        idPost,
+      }),
+     })
+  }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { getPosts } from "./api.js";
+import { getPosts, addPost, getUserPosts, addLike, addDislike } from "./api.js";
 import { renderAddPostPageComponent } from "./components/add-post-page-component.js";
 import { renderAuthPageComponent } from "./components/auth-page-component.js";
 import {
@@ -15,6 +15,8 @@ import {
   removeUserFromLocalStorage,
   saveUserToLocalStorage,
 } from "./helpers.js";
+
+import { renderUserPageComponent } from "./components/user-post-component.js";
 
 export let user = getUserFromLocalStorage();
 export let page = null;
@@ -34,6 +36,7 @@ export const logout = () => {
 /**
  * Включает страницу приложения
  */
+
 export const goToPage = (newPage, data) => {
   if (
     [
@@ -43,9 +46,10 @@ export const goToPage = (newPage, data) => {
       USER_POSTS_PAGE,
       LOADING_PAGE,
     ].includes(newPage)
+          // Если пользователь не авторизован, то отправляем его на авторизацию перед добавлением поста
+
   ) {
     if (newPage === ADD_POSTS_PAGE) {
-      // Если пользователь не авторизован, то отправляем его на авторизацию перед добавлением поста
       page = user ? ADD_POSTS_PAGE : AUTH_PAGE;
       return renderApp();
     }
@@ -54,7 +58,7 @@ export const goToPage = (newPage, data) => {
       page = LOADING_PAGE;
       renderApp();
 
-      return getPosts({ token: getToken() })
+      return getPosts({ token: getToken(),})
         .then((newPosts) => {
           page = POSTS_PAGE;
           posts = newPosts;
@@ -67,11 +71,18 @@ export const goToPage = (newPage, data) => {
     }
 
     if (newPage === USER_POSTS_PAGE) {
-      // TODO: реализовать получение постов юзера из API
-      console.log("Открываю страницу пользователя: ", data.userId);
-      page = USER_POSTS_PAGE;
-      posts = [];
-      return renderApp();
+// TODO: реализовать получение постов юзера из API
+//      console.log("Открываю страницу пользователя: ", data.userId);
+// Делаем страницу загрузки
+      page = LOADING_PAGE;
+      renderApp();
+// Загружаем страницу с постом пользователя
+      let userId = data.userId;
+      return getUserPosts(userId).then((newPosts) => {
+        page = USER_POSTS_PAGE;
+        posts = newPosts;
+        renderApp();
+      });
     }
 
     page = newPage;
@@ -80,6 +91,7 @@ export const goToPage = (newPage, data) => {
     return;
   }
 
+  likeEventListener();
   throw new Error("страницы не существует");
 };
 
@@ -110,9 +122,14 @@ const renderApp = () => {
     return renderAddPostPageComponent({
       appEl,
       onAddPostClick({ description, imageUrl }) {
-        // TODO: реализовать добавление поста в API
-        console.log("Добавляю пост...", { description, imageUrl });
-        goToPage(POSTS_PAGE);
+// TODO: реализовать добавление поста в API
+        addPost({
+          description,
+          imageUrl,
+          token: getToken(),
+        }).then(() => {
+          goToPage(POSTS_PAGE);
+        });
       },
     });
   }
@@ -124,10 +141,81 @@ const renderApp = () => {
   }
 
   if (page === USER_POSTS_PAGE) {
-    // TODO: реализовать страницу фотографию пользвателя
-    appEl.innerHTML = "Здесь будет страница фотографий пользователя";
-    return;
+    return renderUserPageComponent({
+      appEl,
+    });
+  }
+  likeEventListener();
+};
+
+// Декларируем вспомогательную функцию задержки
+function delay(interval) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, interval);
+  });
+}
+
+// Делаем обработчик события click на все кнопки "лайк" на странице
+export const likeEventListener = () => {
+  const likeButtonElements = document.querySelectorAll(".like-button");
+  for (const likeButtonElement of likeButtonElements) {
+    const index = +(likeButtonElement.dataset.index);
+    
+    likeButtonElement.addEventListener("click", () => {
+      let userId = posts[index].id;
+      let idPost = posts[index].idPost;
+// Проверяем, авторизован ли пользователь
+      if (!user) {
+        alert("Если хотите поставить лайк, авторизуйтесь, пожалуйста!")
+        return;
+    }
+
+    if (posts[index].isLiked === true) {
+      delay(500) // добавляем задержку в 500 миллисекунд
+        .then(() => addDislike({idPost, token: getToken()}))
+        .then(() => {
+          if (page === POSTS_PAGE) {
+            return getPosts({ token: getToken() })
+            .then((newPosts) => {
+              page = POSTS_PAGE;
+              posts = newPosts;
+              renderApp();
+            });
+          } else {
+            return getUserPosts({userId, token: getToken()})
+            .then((newPosts) => {
+                page = USER_POSTS_PAGE;
+                posts = newPosts;
+                renderApp();
+              });
+          }
+        });
+    } else {
+      delay(500) // добавляем задержку в 500 миллисекунд
+      .then(() => addLike({ idPost, token: getToken() }))
+      .then(() => {
+        if (page === POSTS_PAGE) {
+          return getPosts({token: getToken()})
+          .then((newPosts) => {
+            page = POSTS_PAGE;
+            posts = newPosts;
+            renderApp();
+          });
+        } else {
+          return getUserPosts({userId, token: getToken()})
+          .then((newPosts) => {
+              page = USER_POSTS_PAGE;
+              posts = newPosts;
+              renderApp();
+              });
+          }
+        });
+      }
+    });
   }
 };
 
 goToPage(POSTS_PAGE);
+likeEventListener();
